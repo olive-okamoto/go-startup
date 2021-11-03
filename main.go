9306aa19206c0784f1c21679455baec6538f80af
Ryo Okamoto <oliveoil0211@gmail.com>
2021-11-03 18:25:21 +0900

    Render template file

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
-	})
+const portNumber = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil)
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplete(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplete(w, "about.page.tmpl")
+}
+
+func renderTemplete(w http.ResponseWriter, tmpl string) {
+	parsedTemplete, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplete.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	_ = http.ListenAndServe(portNumber, nil)
 }
