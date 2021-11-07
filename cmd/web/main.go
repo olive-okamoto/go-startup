60c4c07b17da993877d3f547acbae1c1f3bfb542
Ryo Okamoto <oliveoil0211@gmail.com>
2021-11-07 17:43:45 +0900

    Set up session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/olive-okamoto/go-startup/pkg/config"
 	"github.com/olive-okamoto/go-startup/pkg/handlers"
 	"github.com/olive-okamoto/go-startup/pkg/render"
@@ -11,8 +13,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
