4375317408ddd8a6b6efe20c72930ff0e28b7eff
Ryo Okamoto <oliveoil0211@gmail.com>
2021-11-07 18:08:33 +0900

    Add middleware to load and save the session on every request

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -6,12 +6,11 @@ import (
 	"github.com/justinas/nosurf"
 )
 
-func WriteToConsole(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
+/**
+*	Middleware handles common processing for all request
+ */
 
+// Adds CSRF protection to all POST request
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
@@ -22,4 +21,9 @@ func NoSurf(next http.Handler) http.Handler {
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// Loads and saves the session oon every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
