8d7c474f19216e2bbb7cf18e15d374ff702264f5
Ryo Okamoto <oliveoil0211@gmail.com>
2021-11-07 17:13:37 +0900

    Add miiddleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
